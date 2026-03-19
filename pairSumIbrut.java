245f4bd6529119de2d8d8c4aefc442c7dcb99d49
Gayatri Kulkarni <gaytrikulkarni616@gmail.com>
2026-03-19 18:38:36 +0530

    Update pairSumIbrut.java

--- pairSumIbrut.java
+++ pairSumIbrut.java
@@ -11,7 +11,7 @@ public class arraylist{
         return false;
     }
     public static  void main(String args[]){
-        
+            ArrayList<Integer> list=new ArrayList<>();
             list.add(1);
             list.add(2);
             list.add(3);
